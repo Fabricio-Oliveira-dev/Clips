733d303f54dce45d80cf9b6d2745c59bf56a4e1f
Fabricio Pita <fabricio001400@gmail.com>
2023-05-07 18:31:37 -0300

    Unit testing and E2E testing added to the project

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NavComponent } from './nav.component';
+import { AuthService } from '../services/auth.service';
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj('AuthService', [
+    'createUser', 'logout'
+  ], {
+    isAuthenticated$: of(true),
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: AuthService, useValue: mockedAuthService }
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +34,18 @@ describe('NavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should logout', () => {
+    const logoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
+
+    expect(logoutLink).withContext('Not logged in').toBeTruthy();
+
+    logoutLink.triggerEventHandler('click');
+
+    const service = TestBed.inject(AuthService);
+
+    expect(service.logout)
+    .withContext('Could not click logout link')
+    .toHaveBeenCalledTimes(1);
+  })
 });
--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { TabComponent } from './tab.component';
 
 describe('TabComponent', () => {
@@ -22,4 +22,23 @@ describe('TabComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have .hidden class', () => {
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).toBeTruthy();
+  })
+
+  it('should not have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).not.toBeTrue();
+  })
 });
